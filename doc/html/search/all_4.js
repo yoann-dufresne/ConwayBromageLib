f8ea739408b346b198090da86a33825d4ae8cb3a
Murat Okutucu <okutucumurat75@gmail.com>
2020-08-24 14:14:10 +0200

    folder docs with documentation for pages

--- doc/html/search/all_4.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['kmermanipulator_11',['KmerManipulator',['../class_kmer_manipulator.html',1,'']]],
-  ['kmermanipulatoracgt_12',['KmerManipulatorACGT',['../class_kmer_manipulator_a_c_g_t.html',1,'']]],
-  ['kmermanipulatoractg_13',['KmerManipulatorACTG',['../class_kmer_manipulator_a_c_t_g.html',1,'']]]
-];
--- doc/html/search/related_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['metricforispresent_36',['metricForIsPresent',['../class_conway_bromage.html#aae6bbb22d03163f212bc5d01c206d740',1,'ConwayBromage']]]
-];
